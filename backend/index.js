d7816d02571989383ea73a4d2c50e7f07ad121ba
NikhilNamade <nikhilnamade39@gmail.com>
2025-02-02 11:10:13 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,17 @@ const connectTomongoose = require("./db");
 connectTomongoose();
 const cors = require("cors");
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: "https://farmer-gold.vercel.app",
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization', 'auth-token'], // Add 'auth-token' to allowed headers
+    credentials: true
+}));
 app.use(express.json());
 
-app.use("/api/auth",require("./route/auth"));
-app.use("/api/product",require("./route/product"));
-app.get("/",(req,res)=>{
+app.use("/api/auth", require("./route/auth"));
+app.use("/api/product", require("./route/product"));
+app.get("/", (req, res) => {
     res.send("hii");
 })
 
